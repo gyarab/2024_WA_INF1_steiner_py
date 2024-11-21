3dccc53cf71931431df5ef266311b97146e3a87f
ASt4Github <adam.steiner@student.gyarab.cz>
2024-11-21 10:51:15 +0100

    Fibbonaci

---
+++ test.py
@@ -0,0 +1,32 @@
+def fibonacci(n):
+    if not isinstance(n, int):
+        return TypeError("Invalid input - Input must be an integer")
+    if n <= 0:
+        return TypeError("Invalid input - Input must be a positive integer")
+    sequence = [0, 1]  # Initialize the sequence with the first two numbers
+    while len(sequence) < n:
+        next_number = sequence[-1] + sequence[-2]  # Calculate the next number
+        sequence.append(next_number)  # Add the next number to the sequence
+    return sequence
+
+# Test the function
+n = -1  # Number of Fibonacci numbers to generate
+result = fibonacci(n)
+print(result)
+
+print("Hello World")
+
+def bubblesort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+    return arr
+
+# Test the function
+arr = [64, 34, 25, 12, 22, 11, 90]
+print(bubblesort(arr))
+
+twoDimArray = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+print(twoDimArray[1][1])
