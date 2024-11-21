7b8f7d4d71e90a323a4158362507758cadf08ca5
ASt4Github <adam.steiner@student.gyarab.cz>
2024-11-21 11:24:59 +0100

    Prime

--- test.py
+++ test.py
@@ -45,6 +45,17 @@ def fahrenheit_to_celsius(fahrenheit):
     celsius = (fahrenheit - 32) * 5/9
     return celsius
 
+def is_prime(n):
+    if not isinstance(n, int):
+        return TypeError("Invalid input - Input must be an integer")
+    if n < 2:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+print(is_prime(7))
 
 """
 
